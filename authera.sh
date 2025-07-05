dfd9a5f8ea193a6a1a48f977bd4e072a80c5bb18
ElewonOS <rutszann@gmail.com>
2025-07-05 21:18:11 +0300

    Update authera.sh

--- authera.sh
+++ authera.sh
@@ -182,6 +182,7 @@ echo -e "${GREEN}📁 To configure the server: $UNTURNED_SERVER_DIR/Servers/Defa
 
 
 #Minecraft server setup steps.
+: <<'END_COMMENT'
 elif [ "$game_type_lower_authera" = "minecraft" ]; then
 read -p "Continue with the server installation? (Y/n):" authera_approval
 authera_approval_lower=$(echo "$authera_approval" | tr '[:upper:]' '[:lower:]')
@@ -224,9 +225,9 @@ sudo touch $MINECRAFT_START_FILE
 echo "
 #!/bin/bash 
 java -Xms${mc_ram}M -Xmx${mc_ram}M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar ${mc_jar_lower}-${mc_version_lower}.jar --nogui" > $MINECRAFT_START_FILE
-sudo chmod +x $MINECRAFT_START_FILE
+sudo chmod +x $MINECRAFT_START_FILE --!>
 #Minecraft server setup steps FINISH.
-
+END_COMMENT
 
 
 #Game server setup exit.
